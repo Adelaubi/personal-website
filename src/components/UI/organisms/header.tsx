e0fdb9bae465a08ade139ceedbd01a4529939420
Adelaubi <delaubier.augustin@gmail.com>
2022-08-04 22:09:26 +0200

    Make text responsive in the header

--- src/components/UI/organisms/header.tsx
+++ src/components/UI/organisms/header.tsx
@@ -7,16 +7,16 @@ import Button from '@mui/material/Button';
 
 function ButtonAppBar(): JSX.Element {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box>
       <AppBar position="static" color="primary">
-        <Toolbar>
+        <Toolbar sx={{ height: '10vh' }}>
           <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
             Augustin de Laubier
           </Typography>
-          <Button color="inherit">Présentation</Button>
-          <Button color="inherit">Projets</Button>
-          <Button color="inherit">Compétences</Button>
-          <Button color="inherit">Liens et Contact</Button>
+          <Button color="inherit" sx={{ display: { xs: 'none', sm: 'block' } }}>Présentation</Button>
+          <Button color="inherit" sx={{ display: { xs: 'none', sm: 'block' } }}>Projets</Button>
+          <Button color="inherit" sx={{ display: { xs: 'none', sm: 'block' } }}>Compétences</Button>
+          <Button color="inherit" sx={{ display: { xs: 'none', sm: 'block' } }}>Liens et Contact</Button>
         </Toolbar>
       </AppBar>
     </Box>
